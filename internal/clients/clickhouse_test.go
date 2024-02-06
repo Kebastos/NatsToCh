65e4909d406edf17877ac4b04a58c45248659b7b
Kozyrev.Mikhail <Kozyrev.Mikhail@russianpost.ru>
2024-02-06 07:49:35 +0300

    - fix clickhouse tests

--- internal/clients/clickhouse_test.go
+++ internal/clients/clickhouse_test.go
@@ -2,23 +2,38 @@ package clients_test
 
 import (
 	"context"
+	"github.com/Kebastos/NatsToCh/internal/models"
 	"testing"
+	"time"
 
 	"github.com/Kebastos/NatsToCh/internal/clients"
 	"github.com/Kebastos/NatsToCh/internal/config"
 )
 
+const (
+	tableName = "test"
+)
+
+var testData = []interface{}{
+	&models.DefaultTable{
+		Subject:        "test_subject",
+		CreateDateTime: time.Now(),
+		Content:        "test_data",
+	},
+}
+
+var cfg = &config.CHConfig{
+	Host:            "localhost",
+	Port:            9000,
+	User:            "default",
+	Password:        "",
+	Database:        "test",
+	ConnMaxLifetime: 0,
+	MaxOpenConns:    10,
+	MaxIdleConns:    5,
+}
+
 func TestNewClickhouseClientConnect(t *testing.T) {
-	cfg := &config.CHConfig{
-		Host:            "localhost",
-		Port:            9000,
-		User:            "default",
-		Password:        "",
-		Database:        "default",
-		ConnMaxLifetime: 0,
-		MaxOpenConns:    10,
-		MaxIdleConns:    5,
-	}
 	client := clients.NewClickhouseClient(cfg)
 
 	err := client.Connect()
@@ -28,69 +43,39 @@ func TestNewClickhouseClientConnect(t *testing.T) {
 }
 
 func TestNewClickhouseClientBatchInsertToDefaultSchema(t *testing.T) {
-	cfg := &config.CHConfig{
-		Host:            "localhost",
-		Port:            9000,
-		User:            "default",
-		Password:        "",
-		Database:        "default",
-		ConnMaxLifetime: 0,
-		MaxOpenConns:    10,
-		MaxIdleConns:    5,
-	}
 	client := clients.NewClickhouseClient(cfg)
 	err := client.Connect()
 	if err != nil {
 		t.Errorf("failed to connect to Clickhouse server: %s", err)
 	}
 
-	err = client.BatchInsertToDefaultSchema(context.Background(), "test_table", []interface{}{"test_data"})
+	err = client.BatchInsertToDefaultSchema(context.Background(), tableName, testData)
 	if err != nil {
 		t.Errorf("Failed to batch insert to default schema: %s", err)
 	}
 }
 
 func TestNewClickhouseClientAsyncInsertToDefaultSchema(t *testing.T) {
-	cfg := &config.CHConfig{
-		Host:            "localhost",
-		Port:            9000,
-		User:            "default",
-		Password:        "",
-		Database:        "default",
-		ConnMaxLifetime: 0,
-		MaxOpenConns:    10,
-		MaxIdleConns:    5,
-	}
 	client := clients.NewClickhouseClient(cfg)
 	err := client.Connect()
 	if err != nil {
 		t.Errorf("failed to connect to Clickhouse server: %s", err)
 	}
 
-	err = client.AsyncInsertToDefaultSchema(context.Background(), "test_table", []interface{}{"test_data"}, true)
+	err = client.AsyncInsertToDefaultSchema(context.Background(), tableName, testData, true)
 	if err != nil {
 		t.Errorf("Failed to async insert to default schema: %s", err)
 	}
 }
 
 func TestNewClickhouseClientAsyncInsertToDefaultSchemaNoData(t *testing.T) {
-	cfg := &config.CHConfig{
-		Host:            "localhost",
-		Port:            9000,
-		User:            "default",
-		Password:        "",
-		Database:        "default",
-		ConnMaxLifetime: 0,
-		MaxOpenConns:    10,
-		MaxIdleConns:    5,
-	}
 	client := clients.NewClickhouseClient(cfg)
 	err := client.Connect()
 	if err != nil {
 		t.Errorf("failed to connect to Clickhouse server: %s", err)
 	}
 
-	err = client.AsyncInsertToDefaultSchema(context.Background(), "test_table", []interface{}{}, true)
+	err = client.AsyncInsertToDefaultSchema(context.Background(), tableName, []interface{}{}, true)
 	if err == nil {
 		t.Errorf("Expected error when async inserting with no data")
 	}
